c7236f379a46b2a3c8e83619a46c5bdc869c031d
kzares <kzares.dev@gmail.com>
2026-01-17 17:40:41 -0500

    Added deployment file

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://kzares-dev.github.io',
   integrations: [mdx(), sitemap()],
   output: 'static',
   markdown: {
